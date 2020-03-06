604f7d038c7019abdcf09d64318f85b715b526ec
Wojtek <vopox07@gmail.com>
2020-03-06 22:22:25 +0100

    Updates

---
+++ precache-manifest.a282f7da0e96b6e0e23baa135805afcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b439a0b7d3cb23b2dc66582e0798f7b9",
+    "url": "/pz-intro-frontend/index.html"
+  },
+  {
+    "revision": "df9a013889412afecf31",
+    "url": "/pz-intro-frontend/static/css/main.e95a4c62.chunk.css"
+  },
+  {
+    "revision": "5ffcc954ecb780171846",
+    "url": "/pz-intro-frontend/static/js/2.05806cee.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pz-intro-frontend/static/js/2.05806cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9a013889412afecf31",
+    "url": "/pz-intro-frontend/static/js/main.e84bf396.chunk.js"
+  },
+  {
+    "revision": "aa9f7f698bc5cfc7d76e",
+    "url": "/pz-intro-frontend/static/js/runtime-main.3375a887.js"
+  }
+]);
